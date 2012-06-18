8f450f63cc484ad45e0568f2490bcf76dd5d441e
tester22 <anton@MacBook-Pro.local>
2012-06-18 23:42:29 +0200

    Removed the CLI script

--- copy_plugin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Removing"
-rm -r ~/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/Trakttv.bundle
-echo "Copy new version"
-cp -r Trakttv.bundle ~/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/
-echo "Done!"
