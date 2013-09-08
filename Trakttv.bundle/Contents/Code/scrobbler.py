857e8c25f0474737ada6cbc488b14d43ca213d11
Dean Gardiner <gardiner91@gmail.com>
2013-09-09 11:53:23 +1200

    Added "scrobble_users" setting to filter scrobbling to only certain plex users.

--- Trakttv.bundle/Contents/Code/scrobbler.py
+++ Trakttv.bundle/Contents/Code/scrobbler.py
@@ -11,27 +11,39 @@ class Scrobbler:
     def __init__(self):
         self.trakt = Trakt()
 
+    def stop_playing(self):
+    # If the nothing is currently playing and this is not the first pass, mark the last item as stopped
+        if self.playing is not None:
+            self.playing['state'] = "stopped"
+
+            self.trakt.submit(**self.playing)
+
+            self.playing = None
+
     def update_playing(self):
         xml_file = HTTP.Request(PLAYING_URL)
         xml_content = XML.ElementFromString(xml_file).xpath('//Video')
 
-        try:
-            player = xml_content[0].find('Player')
+        scrobble_users = Prefs['scrobble_users']
+        scrobble_users = [su.strip() for su in scrobble_users.split(',')] if scrobble_users else None
 
-            self.playing = {
-                'key': xml_content[0].get('ratingKey'),
-                'state': player.get('state'),
-                'progress': round(float(xml_content[0].get('viewOffset')) / int(xml_content[0].get('duration')) * 100, 0)
-            }
-            self.trakt.submit(**self.playing)
-        except:  # TODO replace 'except' with proper checks
-            # If the nothing is currently playing and this is not the first pass, mark the last item as stopped
-            if self.playing is not None:
-                self.playing['state'] = "stopped"
+        for media_container in xml_content:
+            try:
+                player = media_container.find('Player')
+                user = media_container.find('User')
 
-                self.trakt.submit(**self.playing)
+                if not scrobble_users or user.get('title') in scrobble_users:
+                    self.playing = {
+                        'key': media_container.get('ratingKey'),
+                        'state': player.get('state'),
+                        'progress': round(float(media_container.get('viewOffset')) / int(media_container.get('duration')) * 100, 0)
+                    }
+                    self.trakt.submit(**self.playing)
+                else:
+                    Log('User "%s" not in Scrobble users list, ignoring' % user.get('title'))
 
-                self.playing = None
+            except:  # TODO replace 'except' with proper checks
+                self.stop_playing()
 
     @route('/applications/trakttv/scrobble_poll')
     def poll(self):
