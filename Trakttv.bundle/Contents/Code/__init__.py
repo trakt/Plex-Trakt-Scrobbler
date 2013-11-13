428daced487d667330675053f6a93967348c8cdb
Dean Gardiner <gardiner91@gmail.com>
2013-11-13 20:01:36 +1300

    Stale or invalid sessions are now cleaned up on startup

--- Trakttv.bundle/Contents/Code/__init__.py
+++ Trakttv.bundle/Contents/Code/__init__.py
@@ -16,6 +16,7 @@ from core.pms import PMS
 from core.trakt import Trakt
 from core.update_checker import UpdateChecker
 from sync import SyncTrakt, ManuallySync, CollectionSync
+from datetime import datetime
 
 
 class Main:
@@ -26,7 +27,10 @@ class Main:
 
         Header.show(self)
 
-        if not 'nowPlaying' in Dict:
+        if 'nowPlaying' in Dict and type(Dict['nowPlaying']) is dict:
+            self.cleanup()
+            Dict.Save()
+        else:
             Dict['nowPlaying'] = dict()
 
         Main.update_config()
@@ -67,6 +71,40 @@ class Main:
     def update_collection(item_id, action):
         # delay sync to wait for metadata
         Thread.CreateTimer(120, CollectionSync, True, item_id, 'add')
+
+    @staticmethod
+    def cleanup():
+        Log.Debug('Cleaning up stale or invalid sessions')
+
+        for key, session in Dict['nowPlaying'].items():
+            delete = False
+
+            # Destroy invalid sessions
+            if type(session) is not dict:
+                delete = True
+            elif 'update_required' not in session:
+                delete = True
+            elif 'last_updated' not in session:
+                delete = True
+            elif type(session['last_updated']) is not datetime:
+                delete = True
+            elif (datetime.now() - session['last_updated']).total_seconds() / 60 / 60 > 24:
+                # Destroy sessions last updated over 24 hours ago
+                Log.Debug('Session %s was last updated over 24 hours ago, queued for deletion', key)
+                delete = True
+
+            # Delete session or flag for update
+            if delete:
+                Log.Info('Session %s looks stale or invalid, deleting it now', key)
+                del Dict['nowPlaying'][key]
+            elif not session['update_required']:
+                Log.Info('Queueing session %s for update', key)
+                session['update_required'] = True
+
+                # Update session in storage
+                Dict['nowPlaying'][key] = session
+
+        Log.Debug('Finished cleaning up')
 
 
 def Start():
