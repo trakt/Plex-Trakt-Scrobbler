68116249ae5ec4875081a43fdb041a4c6d4432df
Dean Gardiner <gardiner91@gmail.com>
2013-12-18 00:42:43 +1300

    Added shell sync menu to trigger sync manager tasks.

--- Trakttv.bundle/Contents/Code/interface/sync_menu.py
+++ Trakttv.bundle/Contents/Code/interface/sync_menu.py
@@ -1,3 +1,70 @@
+from core.helpers import SyncDownString, SyncUpString, finditems, iterget, extend, matches, all, itersections
+from plex.media_server import PMS
+from sync.manager import SyncManager
+
+
 @route('/applications/trakttv/sync')
 def SyncMenu():
-    return
+    if Prefs['username'] is None:
+        return MessageContainer("Error", "No login information entered.")
+
+    oc = ObjectContainer(title2=L("Sync"))
+    all_keys = []
+
+    oc.add(DirectoryObject(
+        key=Callback(Synchronize),
+        title='Synchronize items in ALL sections with trakt.',
+        summary='Synchronize your ' + SyncDownString() + ' items with trakt.',
+        thumb=R("icon-sync.png")
+    ))
+
+    for _, key, title in itersections(PMS.get_sections()):
+        oc.add(DirectoryObject(
+            key=Callback(Push, key=[key]),
+            title='Push items in "' + title + '" to trakt.',
+            summary='Push your ' + SyncUpString() + ' in the "' + title + '" section to trakt.',
+            thumb=R("icon-sync_up.png")
+        ))
+        all_keys.append(key)
+
+    if len(all_keys) > 1:
+        oc.add(DirectoryObject(
+            key=Callback(Push, key=",".join(all_keys)),
+            title='Push items in ALL sections to trakt.',
+            summary='Push your ' + SyncUpString() + ' items in all sections to trakt.',
+            thumb=R("icon-sync_up.png")
+        ))
+
+    oc.add(DirectoryObject(
+        key=Callback(Pull),
+        title='Pull items from Trakt.tv',
+        summary='Pull your ' + SyncDownString() + ' items from trakt.',
+        thumb=R("icon-sync_down.png")
+    ))
+
+    return oc
+
+
+@route('/applications/trakttv/sync/synchronize')
+def Synchronize():
+    if not SyncManager.trigger_synchronize():
+        return MessageContainer('Unable to sync', 'Sync process already running, unable to start')
+
+    return MessageContainer('Syncing started', 'Synchronize has started and will continue in the background')
+
+
+
+@route('/applications/trakttv/sync/push')
+def Push():
+    if not SyncManager.trigger_push():
+        return MessageContainer('Unable to sync', 'Sync process already running, unable to start')
+
+    return MessageContainer('Syncing started', 'Push has been triggered and will continue in the background')
+
+
+@route('/applications/trakttv/sync/pull')
+def Pull():
+    if not SyncManager.trigger_pull():
+        return MessageContainer('Unable to sync', 'Sync process already running, unable to start')
+
+    return MessageContainer('Syncing started', 'Pull has been triggered and will continue in the background')
