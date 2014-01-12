875919b3b5fc8ec80fb635079d58758948e3c2fd
Dean Gardiner <gardiner91@gmail.com>
2014-01-12 22:54:28 +1300

    SyncBase.run now just uses trigger and the new trigger_children methods.

--- Trakttv.bundle/Contents/Code/sync/sync_base.py
+++ Trakttv.bundle/Contents/Code/sync/sync_base.py
@@ -58,41 +58,44 @@ class SyncBase(Base):
         # Activate children and create dictionary map
         self.children = dict([(x.key, x()) for x in self.children])
 
-    def run(self):
-        # Run sub functions (starting with 'run_')
-        sub_functions = [(x, getattr(self, x)) for x in dir(self) if x.startswith('run_')]
+    def run(self, *args, **kwargs):
+        self.trigger(None, *args, **kwargs)
+        self.trigger_children(*args, **kwargs)
 
-        for name, func in sub_functions:
+    def child(self, name):
+        return self.children.get(name)
+
+    def trigger(self, funcs=None, *args, **kwargs):
+        single = kwargs.pop('single', False)
+
+        results = []
+
+        if funcs is None:
+            funcs = [x[4:] for x in dir(self) if x.startswith('run_')]
+        elif type(funcs) is not list:
+            funcs = [funcs]
+
+        for name in funcs:
+            func = getattr(self, 'run_' + name, None)
+
+            if func is None:
+                raise ValueError('Unable to find sub-function with the name "%s"' % name)
+
             log.debug('Running sub-function in task %s with name "%s"' % (self, name))
-            func()
+            results.append(func(*args, **kwargs))
 
-        # Run child tasks
+        if single:
+            return results[0]
+
+        return results
+
+    def trigger_children(self, *args, **kwargs):
         for key, child in self.children.items():
             if not child.auto_run:
                 continue
 
             log.debug('Running child task %s' % child)
-            child.run()
-
-    def trigger(self, key, sub_name=None, *args, **kwargs):
-        if key not in self.children:
-            log.warn('Unable to find task to trigger with key "%s"', key)
-            return None
-
-        task = self.children[key]
-
-        if not sub_name:
-            log.debug('Running child task %s' % task)
-            return task.run(*args, **kwargs)
-
-        if not hasattr(task, 'run_' + sub_name):
-            log.warn('Unable to find sub-function with name "%s"' % sub_name)
-            return None
-
-        func = getattr(task, 'run_' + sub_name)
-
-        log.debug('Running sub-function in task %s with name "%s"' % (task, sub_name))
-        return func(*args, **kwargs)
+            child.run(*args, **kwargs)
 
     @staticmethod
     def update_progress(current, start=0, end=100):
