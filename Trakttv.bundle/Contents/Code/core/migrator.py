f991c2bd84465e0cf827bfd12f4f5fc3a7022cac
Dean Gardiner <gardiner91@gmail.com>
2014-01-26 01:53:01 +1300

    [Issue #102] Fixed "sync" / "sync.py" conflict when doing an unclean upgrade to v0.8+

--- Trakttv.bundle/Contents/Code/core/migrator.py
+++ Trakttv.bundle/Contents/Code/core/migrator.py
@@ -25,4 +25,27 @@ class Migration(object):
     def code_path(self):
         return Core.code_path
 
+
+class SyncMigration(Migration):
+    def run(self):
+        if PLUGIN_VERSION_BASE >= (0, 8):
+            self.upgrade()
+        else:
+            self.downgrade()
+
+    def upgrade(self):
+        sync_path = os.path.join(self.code_path, 'sync.py')
+
+        if os.path.exists(sync_path) and os.path.isfile(sync_path):
+            log.debug('Removing "sync.py" file')
+            os.remove(sync_path)
+
+    def downgrade(self):
+        sync_path = os.path.join(self.code_path, 'sync')
+
+        if os.path.exists(sync_path) and os.path.isdir(sync_path):
+            log.debug('Removing "sync" folder')
+            shutil.rmtree(sync_path)
+
+Migrator.register(SyncMigration)
 Migrator.run()
