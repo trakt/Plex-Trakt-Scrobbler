0832adf396a1d28a2df18627d9683aebeca6353f
Dean Gardiner <gardiner91@gmail.com>
2014-02-27 17:55:04 +1300

    Moved 'get_mapping' and 'get_key' from plex_library to plex_metadata, added support for MCM TV metadata

--- Trakttv.bundle/Contents/Code/plex/metadata.py
+++ Trakttv.bundle/Contents/Code/plex/metadata.py
@@ -3,8 +3,23 @@ from core.eventing import EventManager
 from core.logger import Logger
 from plex.plex_base import PlexBase
 from plex.plex_objects import PlexParsedGuid
+import re
 
 log = Logger('plex.metadata')
+
+# Mappings for agents to their compatible service
+METADATA_AGENT_MAP = {
+    # Multi
+    'mcm':              ('thetvdb', r'MCM_TV_A_(.*)'),
+
+    # Movie
+    'xbmcnfo':          'imdb',
+
+    # TV
+    'abstvdb':          'thetvdb',
+    'thetvdbdvdorder':  'thetvdb',
+    'xbmcnfotv':        'thetvdb',
+}
 
 
 class PlexMetadata(PlexBase):
@@ -17,6 +32,21 @@ class PlexMetadata(PlexBase):
         EventManager.subscribe('notifications.timeline.finished', cls.timeline_finished)
 
         cls.cache.on_refresh.subscribe(cls.on_refresh)
+
+        # Compile agent mapping patterns
+        for key, value in METADATA_AGENT_MAP.items():
+            # Transform into tuple of length 2
+            if type(value) is str:
+                value = (value, None)
+            elif type(value) is tuple and len(value) == 1:
+                value = (value, None)
+
+            # Compile pattern
+            if value[1]:
+                value = (value[0], re.compile(value[1], re.IGNORECASE))
+
+            # Update dictionary
+            METADATA_AGENT_MAP[key] = value
 
     @classmethod
     def on_refresh(cls, key):
@@ -43,6 +73,42 @@ class PlexMetadata(PlexBase):
             guid = cls.get_guid(key)
 
         return PlexParsedGuid.from_guid(guid)
+
+    @classmethod
+    def get_mapping(cls, agent):
+        # Strip leading key
+        agent = agent[agent.rfind('.') + 1:]
+
+        # Return if there is no mapping present
+        if agent not in METADATA_AGENT_MAP:
+            return agent, None
+
+        # Return mapped agent and sid_pattern (if present)
+        return METADATA_AGENT_MAP.get(agent)
+
+    @classmethod
+    def get_key(cls, key):
+        parsed_guid = cls.get_parsed_guid(key=key)
+
+        # Ensure service id is valid
+        if not parsed_guid or not parsed_guid.sid:
+            log.warn('Missing GUID or service identifier for item with ratingKey "%s" (parsed_guid: %s)', key, parsed_guid)
+            return None, None
+
+        agent, sid_pattern = cls.get_mapping(parsed_guid.agent)
+
+        # Match sid with regex
+        if sid_pattern:
+            match = sid_pattern.match(parsed_guid.sid)
+
+            if not match:
+                log.warn('Failed to match "%s" against sid_pattern for "%s" agent', parsed_guid.sid, parsed_guid.agent)
+                return None, None
+
+            # Update with new sid
+            parsed_guid.sid = ''.join(match.groups())
+
+        return parsed_guid, (agent, parsed_guid.sid)
 
     @classmethod
     def timeline_created(cls, item):
--- Trakttv.bundle/Contents/Code/plex/plex_library.py
+++ Trakttv.bundle/Contents/Code/plex/plex_library.py
@@ -8,59 +8,11 @@ from plex.media_server_new import PlexMediaServer
 log = Logger('plex.plex_library')
 
 
-# Mappings for agents to their compatible service
-METADATA_AGENT_MAP = {
-    # Movie
-    'xbmcnfo': 'imdb',
-
-    # TV
-    'abstvdb': 'thetvdb',
-    'thetvdbdvdorder': 'thetvdb',
-    'xbmcnfotv': 'thetvdb',
-}
-
-
 class PlexLibrary(PlexBase):
-    @classmethod
-    def get_mapping(cls, agent):
-        # Strip leading key
-        agent = agent[agent.rfind('.') + 1:]
-
-        # Return if there is no mapping present
-        if agent not in METADATA_AGENT_MAP:
-            return agent, None
-
-        # Return mapped agent and sid_pattern (if present)
-        mapping = METADATA_AGENT_MAP.get(agent)
-
-        if type(mapping) is not tuple:
-            mapping = (mapping, None)
-
-        if len(mapping) == 1:
-            return mapping[0], None
-
-        return mapping
-
-    @classmethod
-    def get_key(cls, key):
-        parsed_guid = PlexMetadata.get_parsed_guid(key=key)
-
-        # Ensure service id is valid
-        if not parsed_guid or not parsed_guid.sid:
-            log.warn('Missing GUID or service identifier for item with ratingKey "%s" (parsed_guid: %s)', key, parsed_guid)
-            return None, None
-
-        agent, sid_pattern = cls.get_mapping(parsed_guid.agent)
-
-        if sid_pattern:
-            raise NotImplementedError()
-
-        return parsed_guid, (agent, parsed_guid.sid)
-
     @classmethod
     def map_item(cls, table, data):
         # Get the key for the item
-        parsed_guid, key = cls.get_key(data.get('ratingKey'))
+        parsed_guid, key = PlexMetadata.get_key(data.get('ratingKey'))
         if parsed_guid is None:
             return False
 
