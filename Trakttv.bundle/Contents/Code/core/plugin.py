0951adc235630e628ea009a0cfef24be8c0569ee
Dean Gardiner <gardiner91@gmail.com>
2014-03-25 23:41:35 +1300

    Bumped version to 0.8.1.2

--- Trakttv.bundle/Contents/Code/core/plugin.py
+++ Trakttv.bundle/Contents/Code/core/plugin.py
@@ -1,8 +1,8 @@
 PLUGIN_NAME = 'Plex-Trakt-Scrobbler'
 PLUGIN_IDENTIFIER = 'com.plexapp.plugins.trakttv'
 
-PLUGIN_VERSION_BASE = (0, 8, 1, 1)
-PLUGIN_VERSION_BRANCH = 'develop'
+PLUGIN_VERSION_BASE = (0, 8, 1, 2)
+PLUGIN_VERSION_BRANCH = 'master'
 
 PLUGIN_VERSION = ''.join([
     '.'.join([str(x) for x in PLUGIN_VERSION_BASE]),
