b8928a7ff3b4e1808365017bc99a72d500309cce
Dean Gardiner <gardiner91@gmail.com>
2014-09-23 17:30:37 +1200

    Fixed a bug where an exception would be raised when a full activity match fails

--- Trakttv.bundle/Contents/Code/pts/activity_logging.py
+++ Trakttv.bundle/Contents/Code/pts/activity_logging.py
@@ -178,6 +178,9 @@ class LoggingActivity(ActivityMethod):
             log.warn('Unknown activity type "%s"', activity_type)
             return
 
+        if match is None:
+            match = {}
+
         # Extend match with query info
         self.query(match, header_match.group('query'))
 
@@ -233,8 +236,9 @@ class LoggingActivity(ActivityMethod):
 
     def progress(self):
         data = self.read_parameters()
+
         if not data:
-            return None
+            return {}
 
         # Translate parameters into timeline-style form
         return {
@@ -252,7 +256,7 @@ class LoggingActivity(ActivityMethod):
             line = self.try_read_line(timeout=5)
             if not line:
                 log.warn('Unable to read log file')
-                return None
+                return {}
 
             # Run through each match function to find a result
             match = None
