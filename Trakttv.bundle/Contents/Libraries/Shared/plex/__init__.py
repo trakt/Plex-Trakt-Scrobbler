ee82339ece29c2f1f580b1f7f0b4254a19e2b333
Dean Gardiner <gardiner91@gmail.com>
2014-11-29 21:40:47 +1300

    Updated plex.py

--- Trakttv.bundle/Contents/Libraries/Shared/plex/__init__.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plex/__init__.py
@@ -1,46 +1,11 @@
-from plex.client import PlexClient
-from plex.lib.six import add_metaclass
-from plex.helpers import has_attribute
+import logging
 
-__version__ = '0.6.0'
+log = logging.getLogger(__name__)
+
+__version__ = '0.6.2'
 
 
-class PlexMeta(type):
-    @property
-    def client(cls):
-        if cls._client is None:
-            cls.construct()
-
-        return cls._client
-
-    def __getattr__(self, name):
-        if has_attribute(self, name):
-            return super(PlexMeta, self).__getattribute__(name)
-
-        if self.client is None:
-            self.construct()
-
-        return getattr(self.client, name)
-
-    def __setattr__(self, name, value):
-        if has_attribute(self, name):
-            return super(PlexMeta, self).__setattr__(name, value)
-
-        if self.client is None:
-            self.construct()
-
-        setattr(self.client, name, value)
-
-    def __getitem__(self, key):
-        if self.client is None:
-            self.construct()
-
-        return self.client[key]
-
-@add_metaclass(PlexMeta)
-class Plex(object):
-    _client = None
-
-    @classmethod
-    def construct(cls):
-        cls._client = PlexClient()
+try:
+    from plex.client import Plex
+except Exception as ex:
+    log.warn('Unable to import submodules - %s', ex)
