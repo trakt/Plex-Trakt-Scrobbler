95fc934c8d7068d4e553ef8756d729afffd52888
Dean Gardiner <gardiner91@gmail.com>
2015-01-08 21:36:16 +1300

    Bumped version to 0.9.1.3-beta

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
@@ -2,8 +2,8 @@ PLUGIN_NAME = 'Plex-Trakt-Scrobbler'
 PLUGIN_IDENTIFIER = 'com.plexapp.plugins.trakttv'
 PLUGIN_PREFIX = '/video/trakt'
 
-PLUGIN_VERSION_BASE = (0, 9, 1, 2)
-PLUGIN_VERSION_BRANCH = 'develop'
+PLUGIN_VERSION_BASE = (0, 9, 1, 3)
+PLUGIN_VERSION_BRANCH = 'beta'
 
 PLUGIN_VERSION = ''.join([
     '.'.join([str(x) for x in PLUGIN_VERSION_BASE]),
