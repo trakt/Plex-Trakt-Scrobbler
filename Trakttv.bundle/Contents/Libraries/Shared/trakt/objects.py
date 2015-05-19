fb82981be4e0abd825e8d0b45abd640a0ec08311
Dean Gardiner <gardiner91@gmail.com>
2015-05-19 23:39:10 +1200

    Updated trakt.py

--- Trakttv.bundle/Contents/Libraries/Shared/trakt/objects.py
+++ Trakttv.bundle/Contents/Libraries/Shared/trakt/objects.py
@@ -300,9 +300,9 @@ class Movie(Video):
 
 
 class Rating(object):
-    def __init__(self):
-        self.value = None
-        self.timestamp = None
+    def __init__(self, value=None, timestamp=None):
+        self.value = value
+        self.timestamp = timestamp
 
     @classmethod
     def create(cls, info):
@@ -314,8 +314,14 @@ class Rating(object):
         r.timestamp = from_iso8601(info.get('rated_at'))
         return r
 
+    def __eq__(self, other):
+        if not isinstance(other, Rating):
+            return NotImplemented
+
+        return self.value == other.value and self.timestamp == other.timestamp
+
     def __repr__(self):
-        return '<Rating %s/10 (%s)>' % (self.value, self.timestamp)
+        return 'trakt.objects.Rating(%r, %r)' % (self.value, self.timestamp)
 
     def __str__(self):
         return self.__repr__()
