05b32769eb6392f98bc06b0d8685eea80a493f57
Dean Gardiner <gardiner91@gmail.com>
2015-05-26 16:09:19 +1200

    Cleaned up collection handler for pull syncing
    TFP-7

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/handlers/collection.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/handlers/collection.py
@@ -7,24 +7,17 @@ log = logging.getLogger(__name__)
 
 
 class Base(MediaHandler):
-    pass
+    def pull(self, rating_key, p_settings, t_item):
+        # Nothing to do
+        return True
 
 
 class Movies(Base):
     media = SyncMedia.Movies
 
-    def pull(self, rating_key, p_settings, t_item):
-        log.debug('Movies.pull(%s, %r, %r)', rating_key, p_settings, t_item)
-
-        # Nothing to do
-        return True
-
 
 class Episodes(Base):
     media = SyncMedia.Episodes
-
-    def pull(self, rating_key, p_settings, t_item):
-        log.debug('Episodes.pull(%s, %r, %r)', rating_key, p_settings, t_item)
 
 
 class Collection(DataHandler):
