d624bf8997ed5fbfcd4995c59f99b1af4dadf899
Dean Gardiner <gardiner91@gmail.com>
2015-05-31 15:29:28 +1200

    Added title maps to [plugin.sync.core.enums]
    TFP-28

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/core/enums.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/core/enums.py
@@ -6,6 +6,23 @@ class SyncData(object):
     Watched     = 0x08
     Watchlist   = 0x16
 
+    __titles__ = None
+
+    @classmethod
+    def title(cls, value):
+        if cls.__titles__ is None:
+            # Build titles map
+            cls.__titles__ = {
+                cls.All:           'All',
+                cls.Collection:    'Collection',
+                cls.Playback:      'Playback',
+                cls.Ratings:       'Ratings',
+                cls.Watched:       'Watched',
+                cls.Watchlist:     'Watchlist'
+            }
+
+        return cls.__titles__.get(value)
+
 
 class SyncMedia(object):
     All         = 0x00
@@ -14,11 +31,40 @@ class SyncMedia(object):
     Seasons     = 0x04
     Episodes    = 0x08
 
+    __titles__ = None
+
+    @classmethod
+    def title(cls, value):
+        if cls.__titles__ is None:
+            # Build titles map
+            cls.__titles__ = {
+                cls.All:        'All',
+                cls.Movies:     'Movies',
+                cls.Shows:      'Shows',
+                cls.Seasons:    'Seasons',
+                cls.Episodes:   'Episodes',
+            }
+
+        return cls.__titles__.get(value)
+
 
 class SyncMode(object):
     Full        = 0x00
-
     Pull        = 0x01
     Push        = 0x02
-
     FastPull    = 0x04
+
+    __titles__ = None
+
+    @classmethod
+    def title(cls, value):
+        if cls.__titles__ is None:
+            # Build titles map
+            cls.__titles__ = {
+                cls.Full:       'Full',
+                cls.Pull:       'Pull',
+                cls.Push:       'Push',
+                cls.FastPull:   'Quick Pull'
+            }
+
+        return cls.__titles__.get(value)
