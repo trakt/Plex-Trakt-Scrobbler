902129b59b897830a660a838b1df85e6b343ff4c
Dean Gardiner <gardiner91@gmail.com>
2015-06-16 11:42:22 +1200

    Implemented `PlaybackHandler` base
    TFP-44

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/handlers/playback/pull.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/handlers/playback/pull.py
@@ -1,5 +1,6 @@
 from plugin.sync.core.enums import SyncData, SyncMedia, SyncMode
-from plugin.sync.handlers.core import DataHandler, MediaHandler, bind
+from plugin.sync.handlers.core import DataHandler, bind
+from plugin.sync.handlers.playback.base import PlaybackHandler
 
 from plex import Plex
 import logging
@@ -7,7 +8,7 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class Base(MediaHandler):
+class Base(PlaybackHandler):
     @staticmethod
     def build_action(action, p_item, p_value, t_value, **kwargs):
         data = {}
@@ -36,27 +37,6 @@ class Base(MediaHandler):
 
         data.update(kwargs)
         return data
-
-    @staticmethod
-    def get_operands(p_item, t_item):
-        # Retrieve plex parameters
-        p_duration = p_item.get('part', {}).get('duration')
-        p_view_offset = p_item.get('settings', {}).get('view_offset')
-
-        # Calculate progress in plex (if available)
-        p_progress = None
-
-        if p_duration is not None and p_view_offset is not None:
-            # Calculate progress from duration and view offset
-            p_progress = round((float(p_view_offset) / p_duration) * 100, 2)
-
-        # Retrieve trakt progress from item
-        if type(t_item) is dict:
-            t_progress = t_item.get('progress')
-        else:
-            t_progress = t_item.progress if t_item else None
-
-        return p_progress, t_progress
 
     @staticmethod
     def update_progress(rating_key, time, duration):
--- Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/handlers/playback/push.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/handlers/playback/push.py
@@ -1,13 +1,13 @@
 from plugin.sync.core.enums import SyncData, SyncMedia, SyncMode
-from plugin.sync.handlers.core import bind
-from plugin.sync.handlers.core.base import DataHandler, MediaHandler
+from plugin.sync.handlers.core import DataHandler, bind
+from plugin.sync.handlers.playback.base import PlaybackHandler
 
 import logging
 
 log = logging.getLogger(__name__)
 
 
-class Base(MediaHandler):
+class Base(PlaybackHandler):
     @staticmethod
     def build_action(action, p_guid, p_item, p_value, **kwargs):
         data = {}
@@ -20,27 +20,6 @@ class Base(MediaHandler):
 
         data.update(kwargs)
         return data
-
-    @staticmethod
-    def get_operands(p_item, t_item):
-        # Retrieve plex parameters
-        p_duration = p_item.get('part', {}).get('duration')
-        p_view_offset = p_item.get('settings', {}).get('view_offset')
-
-        # Calculate progress in plex (if available)
-        p_progress = None
-
-        if p_duration is not None and p_view_offset is not None:
-            # Calculate progress from duration and view offset
-            p_progress = round((float(p_view_offset) / p_duration) * 100, 2)
-
-        # Retrieve trakt progress from item
-        if type(t_item) is dict:
-            t_progress = t_item.get('progress')
-        else:
-            t_progress = t_item.progress if t_item else None
-
-        return p_progress, t_progress
 
     def get_action(self, p_value, t_value):
         if p_value is None and t_value is not None:
