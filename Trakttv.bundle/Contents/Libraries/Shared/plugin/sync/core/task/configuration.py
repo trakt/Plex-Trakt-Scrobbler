81f495e87b0e0fde317c3c4861c206b09755d977
Dean Gardiner <gardiner91@gmail.com>
2015-06-26 16:03:41 +1200

    Updated `SyncConfiguration` to display the configuration values before each sync

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/core/task/configuration.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/core/task/configuration.py
@@ -1,27 +1,27 @@
 from plugin.preferences import OPTIONS_BY_KEY
 
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class SyncConfiguration(object):
-    options = [
-        'sync.collection.mode',
-        'sync.collection.clean',
-
-        'sync.ratings.mode',
-        'sync.ratings.conflict',
-
-        'sync.playback.mode',
-        'sync.watched.mode'
-    ]
-
     def __init__(self, task):
         self.task = task
 
         self._options = {}
 
     def load(self, account):
+        log.debug('Sync Configuration:')
+
         # Load options from database
-        for key in self.options:
-            self._options[key] = OPTIONS_BY_KEY[key].get(account)
+        for key, option in OPTIONS_BY_KEY.items():
+            if option.scope == 'account':
+                self._options[key] = option.get(account)
+            elif option.scope == 'server':
+                self._options[key] = option.get()
+
+            log.debug(' - [%s]: %r', key, self._options[key].value)
 
     def __getitem__(self, key):
         return self._options[key].value
