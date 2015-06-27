34061c3e63b56bc8ade294af42e847ceccf6fed6
Dean Gardiner <gardiner91@gmail.com>
2015-06-27 15:57:25 +1200

    Fixed `plugin.sync` import bug

--- Trakttv.bundle/Contents/Code/interface/m_sync.py
+++ Trakttv.bundle/Contents/Code/interface/m_sync.py
@@ -7,7 +7,8 @@ from plugin.core.filters import Filters
 from plugin.core.helpers.variable import normalize
 from plugin.managers import AccountManager
 from plugin.models import Account, SyncResult
-from plugin.sync import Sync, SyncData, SyncMedia, SyncMode, QueueError
+from plugin.sync import SyncData, SyncMedia, SyncMode
+from plugin.sync.main import Sync, QueueError
 
 from ago import human
 from datetime import datetime
--- Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/__init__.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/__init__.py
@@ -1,12 +1,7 @@
 from plugin.sync.core.enums import SyncData, SyncMedia, SyncMode
-from plugin.sync.main import Sync, QueueError, SyncError
 
 __all__ = [
     'SyncData',
     'SyncMedia',
-    'SyncMode',
-
-    'Sync',
-    'QueueError',
-    'SyncError'
+    'SyncMode'
 ]
