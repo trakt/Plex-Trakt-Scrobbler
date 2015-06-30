6bbf66b5543646c767d1ef0a839c78ae9e344039
Dean Gardiner <gardiner91@gmail.com>
2015-06-30 12:49:55 +1200

    Catch request exceptions in `interface.resources`

--- Trakttv.bundle/Contents/Code/interface/resources.py
+++ Trakttv.bundle/Contents/Code/interface/resources.py
@@ -2,7 +2,11 @@ from plugin.core.constants import PLUGIN_PREFIX
 from plugin.managers import AccountManager
 from plugin.models import Account
 
+from requests import RequestException
+import logging
 import requests
+
+log = logging.getLogger(__name__)
 
 
 @route(PLUGIN_PREFIX + '/resources/cover')
@@ -12,14 +16,22 @@ def Cover(account_id):
     if not account.trakt:
         return Redirect(R('art-default.png'))
 
-    # Refresh trakt account details
-    account.trakt.refresh()
+    try:
+        # Refresh trakt account details
+        account.trakt.refresh()
+    except RequestException:
+        log.warn('Unable to refresh trakt account details', exc_info=True)
+        return Redirect(R('art-default.png'))
 
     if account.trakt.cover is None:
         return Redirect(R('art-default.png'))
 
-    # Request cover image
-    response = requests.get(account.trakt.cover)
+    try:
+        # Request cover image
+        response = requests.get(account.trakt.cover)
+    except RequestException:
+        log.warn('Unable to retrieve account cover', exc_info=True)
+        return Redirect(R('art-default.png'))
 
     if response.status_code != 200:
         return Redirect(R('art-default.png'))
@@ -35,9 +47,13 @@ def Thumb(account_id):
         # TODO better account placeholder image
         return Redirect(R('icon-default.png'))
 
-    response = requests.get(account.thumb_url())
+    try:
+        response = requests.get(account.thumb_url())
+    except RequestException:
+        log.warn('Unable to retrieve account thumbnail', exc_info=True)
+        return Redirect(R('icon-default.png'))
 
     if response.status_code != 200:
-        return Redirect(R('art-default.png'))
+        return Redirect(R('icon-default.png'))
 
     return response.content
