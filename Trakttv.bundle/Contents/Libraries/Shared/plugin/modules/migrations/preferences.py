4b36b8d0830b0ef68ff78b778b4e1e3bdf4d4146
Dean Gardiner <gardiner91@gmail.com>
2015-07-02 20:34:31 +1200

    Removed duplicate "server" account creation

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/modules/migrations/preferences.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/modules/migrations/preferences.py
@@ -1,17 +1,9 @@
-from plugin.models import Account
 from plugin.modules.migrations.core.base import Migration
 from plugin.preferences import Preferences
-from plugin.sync.core.enums import SyncActionMode
 
 
 class PreferencesMigration(Migration):
     def run(self):
-        # Ensure "server" account exists
-        try:
-            Account.get(Account.id == 1)
-        except Account.DoesNotExist:
-            Account.create()
-
         # Initialize preferences for administrator
         Preferences.initialize(account=1)
         Preferences.initialize()
