47a3e4967d94a0ee030dd4e1a027b6a6c95b0881
Dean Gardiner <gardiner91@gmail.com>
2015-07-03 20:55:43 +1200

    Added basic "preferences" API service (`list()`)

---
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/api/preferences.py
@@ -0,0 +1,49 @@
+from plugin.api.core.base import Service, expose
+from plugin.preferences import OPTIONS_BY_KEY
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class PreferencesService(Service):
+    __key__ = 'preferences'
+
+    @expose
+    def list(self, account=None):
+        scope = 'account' if account is not None else 'server'
+
+        def retrieve():
+            for key, option in OPTIONS_BY_KEY.items():
+                if option.scope != scope:
+                    continue
+
+                # Retrieve option details from database
+                option = option.get(account)
+
+                # Build option details
+                data = {
+                    'key': key,
+                    'type': option.type,
+
+                    'default': option.default,
+
+                    'group': option.group,
+                    'label': option.label,
+
+                    'value': option.value
+                }
+
+                if option.type == 'enum':
+                    data['choices'] = option.choices
+
+                # Yield option
+                yield data
+
+        options = list(retrieve())
+
+        log.debug(options)
+
+        return {
+            'options': options
+        }
