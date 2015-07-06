4c87a2d9fe46a9c23773f600398f4f19d12d7c6b
Dean Gardiner <gardiner91@gmail.com>
2015-07-06 15:06:20 +1200

    Implemented `account.create` api method
    TFPC-4

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/api/account.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/api/account.py
@@ -1,12 +1,32 @@
 from plugin.api.core.base import Service, expose
+from plugin.api.core.exceptions import ApiError
 from plugin.managers import AccountManager
+
+import apsw
 import logging
+import peewee
 
 log = logging.getLogger(__name__)
 
 
+class NameConflictError(ApiError):
+    code = 'account.name_conflict'
+    message = 'Name conflicts with an existing account'
+
+
 class Account(Service):
     __key__ = 'account'
+
+    @expose
+    def create(self, name):
+        try:
+            AccountManager.create(
+                name=name
+            )
+        except (apsw.ConstraintError, peewee.IntegrityError):
+            raise NameConflictError
+
+        return True
 
     @expose
     def get(self, full=False, **kwargs):
