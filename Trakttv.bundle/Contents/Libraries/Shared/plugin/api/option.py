b51673dace5a3001d01cc5303be6ef801e13e33d
Dean Gardiner <gardiner91@gmail.com>
2015-07-07 15:27:12 +1200

    Implemented `option.update` api method
    TFPC-6, TFPC-7

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/api/option.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/api/option.py
@@ -1,9 +1,30 @@
 from plugin.api.core.base import Service, expose
+from plugin.api.core.exceptions import ApiError
 from plugin.preferences import OPTIONS_BY_KEY
 
 import logging
 
 log = logging.getLogger(__name__)
+
+
+class InvalidScopeError(ApiError):
+    code = 'option.invalid_scope'
+    message = "Option doesn't match the current scope"
+
+
+class InvalidUpdateError(ApiError):
+    code = 'option.invalid_update'
+    message = "Received an invalid update request"
+
+
+class UnknownOptionError(ApiError):
+    code = 'option.unknown_option'
+    message = "Unknown option referenced"
+
+
+class UpdateConflictError(ApiError):
+    code = 'option.update_conflict'
+    message = "Update conflict, options have been changed by another user"
 
 
 class Option(Service):
@@ -41,3 +62,56 @@ class Option(Service):
                 yield data
 
         return list(retrieve())
+
+    @expose
+    def update(self, changes, account=None):
+        scope = 'account' if account is not None else 'server'
+
+        # Validate changes
+        changed = {}
+        ignored = []
+
+        for key, change in changes.items():
+            if 'from' not in change or 'to' not in change:
+                raise InvalidUpdateError
+
+            if key not in OPTIONS_BY_KEY:
+                raise UnknownOptionError
+            # Retrieve option
+            option = OPTIONS_BY_KEY[key]
+
+            if option.scope != scope:
+                raise InvalidScopeError
+
+            # Retrieve option details from database
+            option = option.get(account)
+
+            if option.value != change['from']:
+                raise UpdateConflictError
+
+            # Ensure option has changed
+            if change['from'] == change['to']:
+                ignored.append(key)
+                continue
+
+            changed[key] = change
+
+        # Update options
+        updated = []
+
+        for key, change in changed.items():
+            if key not in OPTIONS_BY_KEY:
+                raise UnknownOptionError
+
+            value = change['to']
+
+            # Update value
+            option = OPTIONS_BY_KEY[key]
+            option.update(value, account)
+
+            updated.append(key)
+
+        return {
+            'updated': updated,
+            'ignored': ignored
+        }
