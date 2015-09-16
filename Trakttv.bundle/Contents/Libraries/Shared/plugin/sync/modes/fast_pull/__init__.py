40080f9a26403ce5d788c63adf394cdabc8479c7
Dean Gardiner <me@dgardiner.net>
2015-09-16 15:11:10 +1200

    Split `FastPull` sync mode into separate modules

---
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/modes/fast_pull/__init__.py
@@ -0,0 +1,29 @@
+from plugin.sync.core.enums import SyncMode
+from plugin.sync.modes.core.base import Mode
+from plugin.sync.modes.fast_pull.movies import Movies
+from plugin.sync.modes.fast_pull.shows import Shows
+
+import elapsed
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class FastPull(Mode):
+    mode = SyncMode.FastPull
+
+    children = [
+        Movies,
+        Shows
+    ]
+
+    @elapsed.clock
+    def run(self):
+        # Fetch changes from trakt.tv
+        self.trakt.refresh()
+
+        # Build key table for lookups
+        self.trakt.build_table()
+
+        # Run children
+        self.execute_children()
---
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/sync/modes/fast_pull/movies.py
@@ -0,0 +1,101 @@
+from plugin.sync.core.constants import GUID_AGENTS
+from plugin.sync.core.enums import SyncMode, SyncMedia
+from plugin.sync.modes.core.base import Mode, log_unsupported, mark_unsupported
+
+from trakt_sync.cache.main import Cache
+import elapsed
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class Movies(Mode):
+    mode = SyncMode.FastPull
+
+    @elapsed.clock
+    def run(self):
+        # Retrieve movie sections
+        p_sections = self.sections('movie')
+
+        # Fetch movies with account settings
+        p_items = self.plex.library.movies.mapped(
+            p_sections,
+            account=self.current.account.plex.key,
+            parse_guid=True
+        )
+
+        # Calculate total number of steps
+        total = 0
+
+        for (media, data), result in self.trakt.changes:
+            if media != SyncMedia.Movies:
+                # Ignore changes that aren't for episodes
+                continue
+
+            data_name = Cache.Data.get(data)
+
+            for count in result.metrics.movies.get(data_name, {}).itervalues():
+                total += count
+
+        # Task started
+        unsupported_movies = {}
+
+        self.current.progress.start(total)
+
+        # Process movies
+        for rating_key, p_guid, p_item in p_items:
+            if not p_guid or p_guid.agent not in GUID_AGENTS:
+                mark_unsupported(unsupported_movies, rating_key, p_guid, p_item)
+                continue
+
+            key = (p_guid.agent, p_guid.sid)
+
+            # Try retrieve `pk` for `key`
+            pk = self.trakt.table.get(key)
+
+            if pk is None:
+                # No `pk` found
+                continue
+
+            for (media, data), result in self.trakt.changes:
+                if media != SyncMedia.Movies:
+                    # Ignore changes that aren't for movies
+                    continue
+
+                if not self.is_data_enabled(data):
+                    # Data type has been disabled
+                    continue
+
+                data_name = Cache.Data.get(data)
+
+                if data_name not in result.changes:
+                    # No changes for collection
+                    continue
+
+                for action, items in result.changes[data_name].items():
+                    t_item = items.get(pk)
+
+                    if t_item is None:
+                        # No item found in changes
+                        continue
+
+                    self.execute_handlers(
+                        SyncMedia.Movies, data,
+                        action=action,
+                        key=rating_key,
+
+                        p_item=p_item,
+                        t_item=t_item
+                    )
+
+                    # Increment one step
+                    self.current.progress.step()
+
+            # Task checkpoint
+            self.checkpoint()
+
+        # Log details
+        log_unsupported(log, 'Found %d unsupported movie(s)\n%s', unsupported_movies)
+
+        # Task stopped
+        self.current.progress.stop()
