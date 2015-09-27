04950cc26b1796b466c1b099d7c571ee379298a8
Dean Gardiner <me@dgardiner.net>
2015-09-27 23:23:03 +1300

    Added options for list syncing in the plex preferences dialog
    TFP-164 #done

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/preferences/options/o_sync/lists/watchlist.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/preferences/options/o_sync/lists/watchlist.py
@@ -1,6 +1,5 @@
 from plugin.preferences.options.core.base import SimpleOption
 from plugin.preferences.options.o_sync.constants import MODE_KEYS_BY_LABEL, MODE_LABELS_BY_KEY, MODE_IDS_BY_KEY
-from plugin.sync.core.enums import SyncMode
 
 import logging
 
@@ -18,30 +17,30 @@ class SyncListsWatchlistOption(SimpleOption):
     label = 'Mode'
     order = 320
 
-    # preference = 'sync_watched'
-    #
-    # def on_database_changed(self, value, account=None):
-    #     if value not in MODE_IDS_BY_KEY:
-    #         log.warn('Unknown value: %r', value)
-    #         return
-    #
-    #     # Map `value` to plex preference
-    #     value = MODE_IDS_BY_KEY[value]
-    #
-    #     # Update preference
-    #     return self._update_preference(value, account)
-    #
-    # def on_plex_changed(self, value, account=None):
-    #     if value not in MODE_KEYS_BY_LABEL:
-    #         log.warn('Unknown value: %r', value)
-    #         return
-    #
-    #     # Map plex `value`
-    #     value = MODE_KEYS_BY_LABEL[value]
-    #
-    #     # Update database
-    #     self.update(value, account, emit=False)
-    #     return value
+    preference = 'sync_watchlist'
+
+    def on_database_changed(self, value, account=None):
+        if value not in MODE_IDS_BY_KEY:
+            log.warn('Unknown value: %r', value)
+            return
+
+        # Map `value` to plex preference
+        value = MODE_IDS_BY_KEY[value]
+
+        # Update preference
+        return self._update_preference(value, account)
+
+    def on_plex_changed(self, value, account=None):
+        if value not in MODE_KEYS_BY_LABEL:
+            log.warn('Unknown value: %r', value)
+            return
+
+        # Map plex `value`
+        value = MODE_KEYS_BY_LABEL[value]
+
+        # Update database
+        self.update(value, account, emit=False)
+        return value
 
 
 class SyncListsWatchlistPlaylistsOption(SimpleOption):
