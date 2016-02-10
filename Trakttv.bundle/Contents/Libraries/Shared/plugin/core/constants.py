bae00aa0a47e0a9f02f2e959ed385a685c306edf
Dean Gardiner <me@dgardiner.net>
2016-02-11 00:39:42 +1300

    Bumped version to 1.0.0.0-beta

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
@@ -6,7 +6,7 @@ PLUGIN_IDENTIFIER = 'com.plexapp.plugins.trakttv'
 PLUGIN_PREFIX = '/video/trakt'
 
 PLUGIN_VERSION_BASE = (1, 0, 0, 0)
-PLUGIN_VERSION_BRANCH = 'develop'
+PLUGIN_VERSION_BRANCH = 'beta'
 
 PLUGIN_VERSION = ''.join([
     '.'.join([str(x) for x in PLUGIN_VERSION_BASE]),
