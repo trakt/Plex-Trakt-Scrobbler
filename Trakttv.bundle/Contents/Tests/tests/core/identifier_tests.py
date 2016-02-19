156e0f6a6ab1d7907aeeb7e35e9488f93f7cddef
Dean Gardiner <me@dgardiner.net>
2016-02-19 23:31:13 +1300

    Updated tests to support new `Guid` structure

--- Trakttv.bundle/Contents/Tests/tests/core/identifier_tests.py
+++ Trakttv.bundle/Contents/Tests/tests/core/identifier_tests.py
@@ -9,26 +9,26 @@ def test_empty():
 
 
 def test_unknown():
-    assert Identifier.get_ids(Guid('example', '123456', None), strict=False) == {}
+    assert Identifier.get_ids(Guid.parse('example://123456'), strict=False) == {}
 
 
 def test_unknown_strict():
-    assert Identifier.get_ids(Guid('example', '123456', None), strict=True) is None
+    assert Identifier.get_ids(Guid.parse('example://123456'), strict=True) is None
 
 
 def test_string():
-    assert Identifier.get_ids('imdb://123456') == {'imdb': '123456'}
+    assert Identifier.get_ids('com.plexapp.agents.imdb://tt123456') == {'imdb': 'tt123456'}
 
 
 def test_imdb():
-    assert Identifier.get_ids(Guid('imdb', '123456', None)) == {'imdb': '123456'}
+    assert Identifier.get_ids(Guid.parse('com.plexapp.agents.imdb://tt123456')) == {'imdb': 'tt123456'}
 
 
 def test_tmdb():
-    assert Identifier.get_ids(Guid('tmdb', '123456', None)) == {'tmdb': 123456}
-    assert Identifier.get_ids(Guid('tmdb', 123456, None)) == {'tmdb': 123456}
+    assert Identifier.get_ids(Guid.parse('com.plexapp.agents.themoviedb://123456')) == {'tmdb': 123456}
+    assert Identifier.get_ids(Guid.parse('com.plexapp.agents.themoviedb://123456')) == {'tmdb': 123456}
 
 
 def test_tvdb():
-    assert Identifier.get_ids(Guid('tvdb', '123456', None)) == {'tvdb': 123456}
-    assert Identifier.get_ids(Guid('tvdb', 123456, None)) == {'tvdb': 123456}
+    assert Identifier.get_ids(Guid.parse('com.plexapp.agents.thetvdb://123456')) == {'tvdb': 123456}
+    assert Identifier.get_ids(Guid.parse('com.plexapp.agents.thetvdb://123456')) == {'tvdb': 123456}
