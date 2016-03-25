d49cf83cb2a9f295177e6809b65bb95e01f78f25
Dean Gardiner <me@dgardiner.net>
2016-03-25 17:49:49 +1300

    Improved error reporting of framework exceptions

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/logger/handlers/error_reporter.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/logger/handlers/error_reporter.py
@@ -8,12 +8,22 @@ from raven.utils import six
 from raven.utils.stacks import iter_stack_frames, label_from_frame
 import datetime
 import logging
+import os
 import platform
+import re
+import sys
 
 log = logging.getLogger(__name__)
 
 
 VERSION = '.'.join([str(x) for x in PLUGIN_VERSION_BASE])
+
+RE_BUNDLE_PATH = re.compile(r"^.*?(?P<path>\w+\.bundle(?:\\|\/).*?)$", re.IGNORECASE)
+RE_TRACEBACK_HEADER = re.compile(r"^Exception (.*?)\(most recent call last\)(.*?)$", re.IGNORECASE | re.DOTALL)
+
+IGNORED_FUNCTIONS = [
+    ('framework.bundle/contents/resources/versions/2/python/framework/code/sandbox.py', '__import__')
+]
 
 PARAMS = {
     # Message processors + filters
@@ -115,9 +125,27 @@ class ErrorReporterHandler(SentryHandler):
 
         # If there's no exception being processed, exc_info may be a 3-tuple of None
         # http://docs.python.org/library/sys.html#sys.exc_info
+        try:
+            exc_info = self._exc_info(record)
+        except Exception, ex:
+            log.info('Unable to retrieve exception info - %s', ex, exc_info=True)
+            exc_info = None
+
         exception_hash = None
 
-        if record.exc_info and all(record.exc_info):
+        if exc_info and len(exc_info) == 3 and all(exc_info):
+            message = handler_kwargs.get('formatted')
+
+            # Replace exception messages with more helpful details
+            if not record.exc_info and message and RE_TRACEBACK_HEADER.match(message):
+                # Generate new record title
+                handler_kwargs['formatted'] = '%s\n\n%s' % (
+                    self._generate_title(record, exc_info),
+                    message
+                )
+            elif not record.exc_info:
+                log.debug("Message %r doesn't match traceback header", message)
+
             # capture the standard message first so that we ensure
             # the event is recorded as an exception, in addition to having our
             # message interface attached
@@ -125,10 +153,10 @@ class ErrorReporterHandler(SentryHandler):
             data.update(handler.capture(**handler_kwargs))
 
             event_type = 'raven.events.Exception'
-            handler_kwargs = {'exc_info': record.exc_info}
+            handler_kwargs = {'exc_info': exc_info}
 
             # Calculate exception hash
-            exception_hash = ErrorHasher.hash(exc_info=record.exc_info)
+            exception_hash = ErrorHasher.hash(exc_info=exc_info)
 
         # HACK: discover a culprit when we normally couldn't
         elif not (data.get('stacktrace') or data.get('culprit')) and (record.name or record.funcName):
@@ -156,6 +184,158 @@ class ErrorReporterHandler(SentryHandler):
             event_type, stack=stack, data=data,
             extra=extra, date=date, **kwargs
         )
+
+    @classmethod
+    def _exc_info(cls, record):
+        if record.exc_info and all(record.exc_info):
+            return record.exc_info
+
+        # Determine if record is a formatted exception
+        message = record.getMessage()
+
+        if message and message.lower().startswith('exception'):
+            return cls._extract_exc_info(record)
+
+        return None
+
+    @staticmethod
+    def _extract_exc_info(record):
+        # Retrieve last exception information
+        exc_info = sys.exc_info()
+
+        # Ensure exception information is valid
+        if not exc_info or len(exc_info) != 3 or not all(exc_info):
+            return None
+
+        # Retrieve exception
+        _, ex, _ = exc_info
+
+        if not hasattr(ex, 'message'):
+            return None
+
+        # Retrieve last line of log record
+        lines = record.message.strip().split('\n')
+        last_line = lines[-1].lower()
+
+        # Ensure exception message matches last line of record message
+        message = ex.message.lower()
+
+        if message not in last_line:
+            log.debug("Ignored exception with message %r, doesn't match line: %r", message, last_line)
+            return None
+
+        return exc_info
+
+    @staticmethod
+    def _traceback_culprit(tb):
+        result = None
+
+        while tb is not None:
+            frame = tb.tb_frame
+            line_num = tb.tb_lineno
+
+            code = frame.f_code
+            path = code.co_filename
+            function_name = code.co_name
+
+            # Retrieve bundle path
+            file_name = path.lower().replace('\\', '/')
+            match = RE_BUNDLE_PATH.match(file_name)
+
+            if match:
+                file_name = match.group('path')
+
+            log.debug('%r', (file_name, function_name))
+
+            # Check if function has been ignored
+            if (file_name, function_name) in IGNORED_FUNCTIONS:
+                # Function has been ignored
+                if tb.tb_next is None:
+                    # No trace left, use previous trace
+                    break
+
+                # Move to next trace
+                tb = tb.tb_next
+                continue
+
+            if tb.tb_next is None:
+                # No traces left, use current trace
+                result = (path, line_num, function_name)
+                break
+
+            # Store current culprit
+            result = (path, line_num, function_name)
+
+            # Move to next trace
+            tb = tb.tb_next
+
+        # Return "best" culprit match
+        return result
+
+    @classmethod
+    def _generate_title(cls, record, exc_info):
+        _, ex, tb = exc_info
+
+        # Try retrieve culprit from traceback
+        try:
+            culprit = cls._traceback_culprit(tb)
+        except Exception, ex:
+            log.info('Unable to retrieve traceback culprit - %s', ex, exc_info=True)
+            culprit = None
+
+        if culprit and len(culprit) == 3:
+            file_path, _, function_name = culprit
+
+            if function_name != '<module>':
+                return 'Exception raised in %s(): %s' % (function_name, ex)
+
+            # Build module name from path
+            try:
+                module = cls._module_name(file_path)
+            except Exception, ex:
+                log.info('Unable to retrieve module name - %s', ex, exc_info=True)
+                module = None
+
+            if not module:
+                return 'Exception raised in <unknown>'
+
+            return 'Exception raised in %s: %s' % (module, ex)
+
+        # Try retrieve culprit from log record
+        if record.funcName:
+            return 'Exception raised in %s()' % (
+                record.funcName
+            )
+
+        log.debug('Unable to generate title for record %r, exc_info: %r', record, exc_info)
+        return 'Exception raised in <unknown>'
+
+    @staticmethod
+    def _module_name(file_path):
+        # Convert to relative path
+        path = file_path.lower()
+        path = path[path.index('trakttv.bundle'):]
+        path = os.path.splitext(path)[0]
+
+        # Split path into fragments
+        fragments = path.split(os.sep)[2:]
+
+        if not fragments or fragments[0] not in ['code', 'libraries']:
+            return None
+
+        # Build module name
+        module = None
+
+        if fragments[0] == 'code':
+            module = '.'.join(fragments)
+        elif fragments[0] == 'libraries':
+            module = '.'.join(fragments[2:])
+
+        # Verify module name was built
+        if not module:
+            return None
+
+        return module
 
 
 # Build client
