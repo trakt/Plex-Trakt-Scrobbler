70af7e41843ef450c3797db3c9d8ec633c1e25d0
Dean Gardiner <me@dgardiner.net>
2016-06-06 23:40:58 +1200

    Fixed an issue injecting pyOpenSSL into requests/urllib3

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/libraries/tests/openssl_.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/libraries/tests/openssl_.py
@@ -12,10 +12,6 @@ class OpenSSL(BaseTest):
         # Try construct SSL context
         ctx = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv23_METHOD)
 
-        # Inject pyopenssl into requests
-        from requests.packages.urllib3.contrib.pyopenssl import inject_into_urllib3
-        inject_into_urllib3()
-
         return {
             'versions': {
                 'pyopenssl': getattr(OpenSSL, '__version__', None)
@@ -24,7 +20,10 @@ class OpenSSL(BaseTest):
 
     @classmethod
     def on_success(cls, metadata):
-        from plugin.core.logger.handlers.error_reporter import RAVEN
+        # Inject pyopenssl into requests
+        from requests.packages.urllib3.contrib.pyopenssl import inject_into_urllib3
+        inject_into_urllib3()
 
         # Enable secure error reporting
+        from plugin.core.logger.handlers.error_reporter import RAVEN
         RAVEN.set_protocol('threaded+requests+https')
