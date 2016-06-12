7560096d7e5bc2cae91a8f33785d267ea47f7823
Dean Gardiner <me@dgardiner.net>
2016-06-12 23:54:31 +1200

    Bumped version to 1.0.3.0

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
@@ -7,8 +7,8 @@ PLUGIN_ICON = 'icon-default.png'
 PLUGIN_IDENTIFIER = 'com.plexapp.plugins.trakttv'
 PLUGIN_PREFIX = '/video/trakt'
 
-PLUGIN_VERSION_BASE = (1, 0, 2, 1)
-PLUGIN_VERSION_BRANCH = 'develop'
+PLUGIN_VERSION_BASE = (1, 0, 3, 0)
+PLUGIN_VERSION_BRANCH = 'master'
 
 PLUGIN_VERSION = ''.join([
     '.'.join([str(x) for x in PLUGIN_VERSION_BASE]),
