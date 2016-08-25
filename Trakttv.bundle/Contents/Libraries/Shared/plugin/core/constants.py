9affbecf4b51e1a04526b8cbfb5d7feb6dc078fb
Dean Gardiner <me@dgardiner.net>
2016-08-25 17:05:02 +1200

    Updated branch name

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/constants.py
@@ -8,7 +8,7 @@ PLUGIN_IDENTIFIER = 'com.plexapp.plugins.trakttv'
 PLUGIN_PREFIX = '/video/trakt'
 
 PLUGIN_VERSION_BASE = (1, 1, 0, 5)
-PLUGIN_VERSION_BRANCH = 'beta'
+PLUGIN_VERSION_BRANCH = 'develop'
 
 PLUGIN_VERSION = ''.join([
     '.'.join([str(x) for x in PLUGIN_VERSION_BASE]),
