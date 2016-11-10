bbd7cf670955a5df0851e1f2101694661871b655
Dean Gardiner <me@dgardiner.net>
2016-11-10 16:33:44 +1300

    Model manager classes will now raise the `PluginDisabledError` exception if any database operation is attempted after a critical error has been encountered

--- Trakttv.bundle/Contents/Libraries/Shared/plugin/core/exceptions.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plugin/core/exceptions.py
@@ -1,2 +1,9 @@
 class AccountAuthenticationError(Exception):
     pass
+
+
+class PluginDisabledError(Exception):
+    def __init__(self):
+        super(PluginDisabledError, self).__init__(
+            'Plugin has been automatically disabled, check the plugin logs for more information'
+        )
