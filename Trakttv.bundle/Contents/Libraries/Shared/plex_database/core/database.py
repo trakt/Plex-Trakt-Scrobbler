fe5cbb5ca581ccea821caebf43d5c9ed4785a030
Dean Gardiner <me@dgardiner.net>
2016-11-18 13:18:07 +1300

    Updated plex.database.py

--- Trakttv.bundle/Contents/Libraries/Shared/plex_database/core/database.py
+++ Trakttv.bundle/Contents/Libraries/Shared/plex_database/core/database.py
@@ -1,9 +1,21 @@
-from playhouse.apsw_ext import APSWDatabase
-import apsw
 import logging
 import os
 
 log = logging.getLogger(__name__)
+
+# Try import "apsw"
+try:
+    import apsw
+except ImportError as ex:
+    log.error('Unable to import "apsw": %s', ex, exc_info=True)
+    apsw = None
+
+# Try import "playhouse.apsw_ext"
+try:
+    from playhouse.apsw_ext import APSWDatabase
+except ImportError as ex:
+    log.error('Unable to import "playhouse.apsw_ext": %s', ex, exc_info=True)
+    APSWDatabase = None
 
 # Locate "com.plexapp.plugins.library.db"
 if os.environ.get('LIBRARY_DB'):
@@ -13,7 +25,7 @@ else:
     db_path = None
 
 # Connect to database
-if db_path:
+if db_path and apsw and APSWDatabase:
     log.debug('Connecting to %r', db_path)
 
     db = APSWDatabase(db_path, flags=apsw.SQLITE_OPEN_READONLY, journal_mode='WAL')
