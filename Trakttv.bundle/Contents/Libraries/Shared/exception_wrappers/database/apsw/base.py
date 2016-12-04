85286e0d914fa6d3032123416aee0dd6aede028d
Dean Gardiner <me@dgardiner.net>
2016-12-04 18:01:09 +1300

    Database exception wrappers will now log every exception at the info level (for debugging purposes)

--- Trakttv.bundle/Contents/Libraries/Shared/exception_wrappers/database/apsw/base.py
+++ Trakttv.bundle/Contents/Libraries/Shared/exception_wrappers/database/apsw/base.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import
 from exception_wrappers.exceptions import DatabaseDisabledError
 from exception_wrappers.libraries import apsw
 from exception_wrappers.manager import ExceptionWrapper
+import logging
 
+log = logging.getLogger(__name__)
 disabled_databases = {}
 
 
@@ -29,6 +31,8 @@ class APSWBaseWrapper(object):
         return disabled_databases.get(self.name) is None
 
     def on_exception(self, source, exc_info):
+        log.info('Exception raised in %s: %s', source, exc_info[1], exc_info=True)
+
         # Raise exception if the database is already disabled
         if disabled_databases.get(self.name):
             raise DatabaseDisabledError(exc_info[1])
