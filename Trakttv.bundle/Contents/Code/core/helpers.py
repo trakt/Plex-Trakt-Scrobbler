950d5f87e794fdb0b421511be4bd01f424411ede
Dean Gardiner <me@dgardiner.net>
2016-12-04 22:34:35 +1300

    Removed some unused helper methods

--- Trakttv.bundle/Contents/Code/core/helpers.py
+++ Trakttv.bundle/Contents/Code/core/helpers.py
@@ -30,133 +30,11 @@ def try_convert(value, value_type, default=None):
         return default
 
 
-def add_attribute(target, source, key, value_type=str, func=None, target_key=None):
-    if target_key is None:
-        target_key = key
-
-    value = try_convert(source.get(key, None), value_type)
-
-    if value:
-        target[target_key] = func(value) if func else value
-
-
-def merge(a, b):
-    a.update(b)
-    return a
-
-
 def all(items):
     for item in items:
         if not item:
             return False
     return True
-
-
-def any(items):
-    for item in items:
-        if item:
-            return True
-
-    return False
-
-
-def json_import():
-    try:
-        import simplejson as json
-
-        log.info("Using 'simplejson' module for JSON serialization")
-        return json, 'json'
-    except ImportError:
-        pass
-
-    # Try fallback to 'json' module
-    try:
-        import json
-
-        log.info("Using 'json' module for JSON serialization")
-        return json, 'json'
-    except ImportError:
-        pass
-
-    # Try fallback to 'demjson' module
-    try:
-        import demjson
-
-        log.info("Using 'demjson' module for JSON serialization")
-        return demjson, 'demjson'
-    except ImportError:
-        log.warn("Unable to find json module for serialization")
-        raise Exception("Unable to find json module for serialization")
-
-# Import json serialization module
-JSON, JSON_MODULE = json_import()
-
-
-# JSON serialization wrappers to simplejson/json or demjson
-def json_decode(s):
-    if JSON_MODULE == 'json':
-        return JSON.loads(s)
-
-    if JSON_MODULE == 'demjson':
-        return JSON.decode(s)
-
-    raise NotImplementedError()
-
-
-def json_encode(obj):
-    if JSON_MODULE == 'json':
-        return JSON.dumps(obj)
-
-    if JSON_MODULE == 'demjson':
-        return JSON.encode(obj)
-
-    raise NotImplementedError()
-
-
-def str_format(s, *args, **kwargs):
-    """Return a formatted version of S, using substitutions from args and kwargs.
-
-    (Roughly matches the functionality of str.format but ensures compatibility with Python 2.5)
-    """
-
-    args = list(args)
-
-    x = 0
-    while x < len(s):
-        # Skip non-start token characters
-        if s[x] != '{':
-            x += 1
-            continue
-
-        end_pos = s.find('}', x)
-
-        # If end character can't be found, move to next character
-        if end_pos == -1:
-            x += 1
-            continue
-
-        name = s[x + 1:end_pos]
-
-        # Ensure token name is alpha numeric
-        if not name.isalnum():
-            x += 1
-            continue
-
-        # Try find value for token
-        value = args.pop(0) if args else kwargs.get(name)
-
-        if value:
-            value = str(value)
-
-            # Replace token with value
-            s = s[:x] + value + s[end_pos + 1:]
-
-            # Update current position
-            x = x + len(value) - 1
-
-        x += 1
-
-    return s
 
 
 def str_pad(s, length, align='left', pad_char=' ', trim=False):
@@ -188,19 +66,6 @@ def str_pad(s, length, align='left', pad_char=' ', trim=False):
 def pad_title(value):
     """Pad a title to 30 characters to force the 'details' view."""
     return str_pad(value, 30, pad_char=' ')
-
-
-def total_seconds(span):
-    return (span.microseconds + (span.seconds + span.days * 24 * 3600) * 1e6) / 1e6
-
-
-def sum(values):
-    result = 0
-
-    for x in values:
-        result = result + x
-
-    return result
 
 
 def timestamp():
@@ -275,52 +140,6 @@ def thread_wrapper(func, args=None, kwargs=None, thread_name=None):
         func(*args, **kwargs)
     except Exception as ex:
         log.error('Exception raised in thread "%s": %s', thread_name, ex, exc_info=True)
-
-
-def schedule(func, seconds, *args, **kwargs):
-    def schedule_sleep():
-        time.sleep(seconds)
-        func(*args, **kwargs)
-
-    spawn(schedule_sleep)
-
-
-def build_repr(obj, keys):
-    key_part = ', '.join([
-        ('%s: %s' % (key, repr(getattr(obj, key))))
-        for key in keys
-    ])
-
-    cls = getattr(obj, '__class__')
-
-    return '<%s %s>' % (getattr(cls, '__name__'), key_part)
-
-
-def plural(value):
-    if type(value) is list:
-        value = len(value)
-
-    if value == 1:
-        return ''
-
-    return 's'
-
-
-def join_attributes(**kwargs):
-    fragments = [
-        (('%s: %s' % (key, value)) if value else None)
-        for (key, value) in kwargs.items()
-    ]
-
-    return ', '.join([x for x in fragments if x])
-
-
-def md5(value):
-    # Generate MD5 hash of key
-    m = hashlib.md5()
-    m.update(value)
-
-    return m.hexdigest()
 
 
 def safe_encode(string):
