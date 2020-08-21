80fa66a2bbc4b21dfbed3cc01d8e14a1abc671ad
RG9400 <rgupta9400@gmail.com>
2020-08-21 13:31:21 -0500

    create taggings DB model (prep for sync)

---
+++ Trakttv.bundle/Contents/Libraries/Shared/plex_database/models/taggings.py
@@ -0,0 +1,26 @@
+from plex_database.core import db
+
+from peewee import *
+from plex_database.models import MetadataItem
+from plex_database.models import Tags
+
+
+class SectionLocation(Model):
+    class Meta:
+        database = db
+        db_table = 'taggings'
+
+    metadata_item = ForeignKeyField(MetadataItem, null=True, related_name='taggings')
+    tag = ForeignKeyField(Tags, null=True, related_name='taggings')
+
+    index = IntegerField(null=True)
+    text = CharField(null=True)
+
+    time_offset = IntegerField(null=True)
+    end_time_offset = IntegerField(null=True)
+
+    thumb_url = CharField(null=True)
+
+    created_at = DateTimeField(null=True)
+    extra_data = CharField(null=True)
+
